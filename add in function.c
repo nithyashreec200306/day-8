5ff9f1210ecb069567d1222b9fbdbc77b963e1e6
nithyashreec200306 <nithyashree2036@gmail.com>
2025-05-20 14:52:50 +0530

    Add files via upload

---
+++ add in function.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int add()
+{
+    int x,y;
+    scanf("%d%d",&x,&y);
+    int sum=x+y;//hera we can use * this operator also with the declaration in tha name of sum
+    int a=50;
+    //return x+y
+}
+    int main()
+    {
+        int x=add();
+        printf("%d",x);
+        printf("%d",add());
+        
+    }
